c1748eec052d4a2f79fd9a52233ebc5955db6226
angeloocana <angeloocana@gmail.com>
2017-06-22 22:19:03 -0400

    Fix links to deleted files

--- pages/_template.js
+++ pages/_template.js
@@ -1,36 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-import '../src/css/github.css'
-import '../src/css/index.css'
-import '../src/css/App.css'
-import '../src/css/ProjectTable.css'
-import '../src/css/social.css'
-import '../src/css/Picture.css'
-import '../src/css/TagCard.css'
-import '../src/css/Header.css'
-import '../src/css/Footer.css'
-import '../src/css/TranslatorSection.css'
-
-// export default ({ children }) => {
-//   console.log('template', children);
-//   return (
-//   <div>
-//     <p>template</p>
-//     {children}
-//   </div>
-// )}
+import '../src/css/github.css';
+import '../src/css/index.css';
+import '../src/css/App.css';
+import '../src/css/social.css';
+import '../src/css/Header.css';
+import '../src/css/Footer.css';
+import '../src/css/TranslatorSection.css';
 
 module.exports = React.createClass({
-  propTypes () {
+  propTypes() {
     return {
       children: React.PropTypes.any
-    }
+    };
   },
-  render () {
+  render() {
     return (
       <div>
         {this.props.children}
       </div>
-    )
+    );
   }
-})
+});
--- src/Page.js
+++ src/Page.js
@@ -2,36 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TranslatorSection from './components/TranslatorSection';
-import Introduction from './components/Introduction';
-import graphFactory from './components/graphFactory';
-import categories from './categories';
 
 class Page extends React.Component {
-  render () {
-    const { entities, url, projects, intl } = this.props;
-    const locale = intl.locale;
-    const factory = graphFactory({ projects, entities, locale });
-    const Graph = (props) => factory.createGraph(props);
+  render() {
+    const { locale } = this.props.intl;
     return (
       <div>
-        <Header language={intl.locale} />
-        <Introduction
-          entities={entities}
-          url={url}
-          intl={intl}
-        />
-        {categories.map((item, i) => (
-          <Graph
-            key={item.tag}
-            tag={item.tag}
-            number={i + 1}
-            title={item.title}
-            excluded={item.excluded}
-            count={item.count}
-          />
-        ))}
-        <TranslatorSection language={intl.locale} />
-        <Footer language={intl.locale} />
+        <Header language={locale} />
+        <TranslatorSection language={locale} />
+        <Footer language={locale} />
       </div>
     );
   }
