77de14716785be1c07fc4cd5638317d9fb7908ae
angeloocana <angeloocana@gmail.com>
2017-07-09 09:45:41 -0400

    Add markdown plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,44 @@
 module.exports = {
     siteMetadata: {
         title: 'Angelo Ocana',
-        authorName: "Angelo Ocana",
-        authorEmail: "angeloocana@gmail.com",
+        author: {
+            name: "Angelo Ocana",
+            email: "angeloocana@gmail.com"
+        },
         linkPrefix: "/angeloocana",
         GA: "UA-61019439-1",
         meta: [
             { "name": "description", "content": "A blog about programming for the web, and various interesting things." },
             { "name": "keywords", "content": "blog, articles, programming, drawing, frontend, backend" },
         ]
-    }
+    },
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages/`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 690,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                ],
+            },
+        },
+        `gatsby-plugin-sharp`,
+    ]
 };
