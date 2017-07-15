6708af66657188cee2a1c2d289214ae55f526b27
Angelo Ocana <angeloocana@gmail.com>
2017-07-15 12:34:52 -0400

    Add backup files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Angelo Ocana Software Development and Training`,
+    author: {
+      name: `Angelo Ocana`,
+      email: `angeloocana@gmail.com`
+    },
+    GA: "UA-61019439-1",
   },
   plugins: [`gatsby-plugin-react-helmet`],
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { rhythm } from '../utils/typography';
+import Bio from './Bio';
+
+export default (props) =>
+  <div
+    style={{
+      marginBottom: rhythm(2.5),
+      borderTop: '1px dashed #ccc',
+      paddingTop: 30,
+    }}
+  >
+    <Bio />
+  </div>;
