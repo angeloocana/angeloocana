f2771cbe64be56aafd390617886793f406580eb8
Angelo Ocana <angeloocana@gmail.com>
2017-07-16 13:27:00 -0400

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import React from 'react';
+// import PropTypes from 'proptypes';
+import graphql from 'graphql';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import typography from '../utils/typography';
+const rhythm = typography.rhythm;
+const scale = typography.scale;
+
+class Header extends React.Component {
+    static propTypes = {
+        // data: PropTypes.object
+    }
+
+    render() {
+        console.log('Header props', this.props);
+
+        // const { siteMetadata } = this.props.data.site;
+
+        const siteMetadata = {
+            header: {
+
+            }
+        };
+
+        return (
+          <div>
+            <Helmet defaultTitle={siteMetadata.title} titleTemplate={`${siteMetadata.header.title} | %s`} />
+            <Link
+              style={{
+                        textDecoration: 'none',
+                        boxShadow: 'none',
+                        color: 'inherit',
+                    }}
+              to="/"
+                >
+              <h1
+                style={{
+                            ...scale(1.5),
+                            marginBottom: rhythm(1),
+                            marginTop: 0,
+                        }}
+                    >
+                        TESTe - {siteMetadata.header.title}
+              </h1>
+              <p>{siteMetadata.header.subTitle}</p>
+            </Link>
+          </div>
+        );
+    }
+}
+
+export default Header;
+
+export const pageQuery = graphql`
+  query LayoutsQuery {
+    site {
+      siteMetadata {
+        header{
+          title,
+          subTitle
+        }
+      }
+    }
+  }
+`;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,82 +1,19 @@
 import React from 'react';
 import PropTypes from 'proptypes';
-import graphql from 'graphql';
-import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
+import Header from '../components/Header';
 import 'typeface-alegreya';
 import 'typeface-alegreya-sans';
 
 import '../css/prism-coy.css';
 import typography from '../utils/typography';
 const rhythm = typography.rhythm;
-const scale = typography.scale;
 
 class Wrapper extends React.Component {
   static propTypes = {
-    // data: PropTypes.object,
-    location: PropTypes.object,
     children: PropTypes.func
   }
 
   render() {
-    console.log('props', this.props);
-
-    // const { siteMetadata } = this.props.data.site;
-
-    const siteMetadata = {
-      header: {
-
-      }
-    };
-
-    let header;
-    // Check if the location is either the front page or a tags page.
-    // If so, use a big header, otherwise use a smaller one.
-    if (
-      ['/', '/tags/'].indexOf(this.props.location.pathname) !== -1 ||
-      this.props.location.pathname.indexOf('/tags/') !== -1
-    ) {
-      header = (
-        <Link
-          style={{
-            textDecoration: 'none',
-            boxShadow: 'none',
-            color: 'inherit',
-          }}
-          to="/"
-        >
-          <h1
-            style={{
-              ...scale(1.5),
-              marginBottom: rhythm(1),
-              marginTop: 0,
-            }}
-          >
-            Bricolage
-          </h1>
-        </Link>
-      );
-    } else {
-      header = (
-        <Link
-          style={{
-            textDecoration: 'none',
-            boxShadow: 'none',
-            color: 'inherit',
-          }}
-          to="/"
-        >
-          <h3
-            style={{
-              marginTop: 0,
-            }}
-          >
-            {siteMetadata.header.title}
-          </h3>
-          <span>{siteMetadata.header.subTitle}</span>
-        </Link>
-      );
-    }
     return (
       <div
         style={{
@@ -85,10 +22,7 @@ class Wrapper extends React.Component {
           margin: '0 auto',
         }}
       >
-        <Helmet defaultTitle={siteMetadata.title} titleTemplate={`${siteMetadata.header.title} | %s`} />
-        <div>
-          {header}
-        </div>
+        <Header />
         {this.props.children()}
       </div>
     );
@@ -96,16 +30,3 @@ class Wrapper extends React.Component {
 }
 
 export default Wrapper;
-
-export const pageQuery = graphql`
-  query LayoutsQuery {
-    site {
-      siteMetadata {
-        header{
-          title,
-          subTitle
-        }
-      }
-    }
-  }
-`;
