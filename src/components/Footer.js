50130f3d4b286977fc315fc6c6abbe296428957d
Angelo Ocana <angeloocana@gmail.com>
2017-07-20 08:11:28 -0400

    Add github link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Gravatar from 'react-gravatar';
+import HeartIcon from 'react-icons/lib/fa/heart';
+import GithubIcon from 'react-icons/lib/fa/github';
+import Link from './Link';
 
 const Footer = () => {
   const author = {
@@ -8,15 +11,18 @@ const Footer = () => {
     email: 'angeloocana@gmail.com'
   };
 
+  const blogGithubLink = 'https://github.com/angeloocana/angeloocana';
+
   return (<footer>
     <Gravatar
       email={author.email}
       alt={author.name}
       width={60}
       height={60} />
-    Written by <strong>{author.name}</strong> {' '}
-    who lives and works in {author.homeCity} building really useful things.
-  You should <a href="https://twitter.com/ocanaangelo" > follow him on Twitter</a>
+    Built with <HeartIcon /> by <Link to={author.defaultLink}>{author.name}</Link>
+    <p>
+      <Link to={blogGithubLink}><GithubIcon /> Do you want to see the source code? It's opensource, It's free, check it out!!!</Link>
+    </p>
   </footer>
   );
 };
