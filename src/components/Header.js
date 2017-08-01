04e16c7229cec36a9dc2fdcca59c8e4593e51c5b
angeloocana <angeloocana@gmail.com>
2017-08-01 15:56:10 -0400

    Add small header when not home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,35 +5,41 @@ import Helmet from 'react-helmet';
 import Menu from './Menu';
 import styled from 'styled-components';
 
+const headerTheme = (props) =>
+  props.isHome
+    ? props.theme.header.isHome
+    : props.theme.header;
+
 const Title = styled(Link)`
   display: block;
-  font-size: ${props => props.theme.header.title.fontSize};
-  text-align: ${props => props.theme.header.title.textAlign};
-  padding: ${props => props.theme.header.title.padding};
-  margin: ${props => props.theme.header.title.margin};
-  line-height: ${props => props.theme.header.title.lineHeight};
+  font-size: ${props => headerTheme(props).title.fontSize};
+  text-align: ${props => headerTheme(props).title.textAlign};
+  padding: ${props => headerTheme(props).title.padding};
+  margin: ${props => headerTheme(props).title.margin};
+  line-height: ${props => headerTheme(props).title.lineHeight};
 `;
 
 const SubTitle = styled.p`
-  font-size: ${props => props.theme.header.subTitle.fontSize};
-  padding: ${props => props.theme.header.subTitle.padding};
-  margin: ${props => props.theme.header.subTitle.margin};
+  font-size: ${props => headerTheme(props).subTitle.fontSize};
+  padding: ${props => headerTheme(props).subTitle.padding};
+  margin: ${props => headerTheme(props).subTitle.margin};
 `;
 
 class Header extends React.Component {
   static propTypes = {
-    siteMetadata: PropTypes.object
+    siteMetadata: PropTypes.object,
+    isHome: PropTypes.bool
   }
 
   render() {
-    const { siteMetadata } = this.props;
+    const { siteMetadata, isHome } = this.props;
 
     return (
       <header>
         <Helmet defaultTitle={siteMetadata.title} titleTemplate={`${siteMetadata.header.title} | %s`} />
-        <Title to="/">
+        <Title to="/" isHome={isHome}>
           {siteMetadata.header.title}
-          <SubTitle>{siteMetadata.header.subTitle}</SubTitle>
+          <SubTitle isHome={isHome}>{siteMetadata.header.subTitle}</SubTitle>
         </Title>
         <Menu />
       </header>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,32 +17,44 @@ const Background = styled.div`
 `;
 
 const BodyContainer = styled.div`
-    font-family: ${props => props.theme.fontFamily};
-    color: ${props => props.theme.color};
-    background-color: ${props => props.theme.bg};
-    padding: ${props => props.theme.padding};
-    margin: ${props => props.theme.margin};
-    max-width: ${props => props.theme.maxWidth};
+  font-family: ${props => props.theme.fontFamily};
+  color: ${props => props.theme.color};
+  background-color: ${props => props.theme.bg};
+  padding: ${props => props.theme.padding};
+  margin: ${props => props.theme.margin};
+  max-width: ${props => props.theme.maxWidth};
 
-    -moz-font-feature-settings: "calt=1", "clig=1", "dlig=1", "kern=1", "liga=1", "salt=1";
-    -moz-font-feature-settings: "calt" 1, "clig" 1, "dlig" 1, "kern" 1, "liga" 1, "salt" 1;
-    -webkit-font-feature-settings: "calt" 1, "clig" 1, "dlig" 1, "kern" 1, "liga" 1, "salt" 1;
-    -ms-font-feature-settings: "calt=1", "clig=1", "dlig=1", "kern=1", "liga=1", "salt=1";
-    -o-font-feature-settings: "calt" 1, "clig" 1, "dlig" 1, "kern" 1, "liga" 1, "salt" 1;
-    font-feature-settings: "calt" 1, "clig" 1, "dlig" 1, "kern" 1, "liga" 1, "salt" 1;
+  font-feature-settings: "calt" 1, "clig" 1, "dlig" 1, "kern" 1, "liga" 1, "salt" 1;
 `;
+
+/**
+ * Gets the number of paths in a url
+ * @param {*} url pathName
+ * @returns {Number} number of paths
+ */
+const nPaths = (url) => (url.match(/\//g) || []).length - 1;
+
+/**
+ * Checks if the url is /, /en/ or /pt/
+ * @param {*} location this.props.location
+ * @returns {Boolean} is home or not
+ */
+const isHomePage = ({pathname}) => nPaths(pathname) <= 1;
 
 class Wrapper extends React.Component {
   static propTypes = {
-    children: PropTypes.func
+    children: PropTypes.func,
+    location: PropTypes.object
   }
 
   render() {
+    const isHome = isHomePage(this.props.location);
+
     return (
       <ThemeProvider theme={theme}>
         <Background>
           <BodyContainer>
-            <Header siteMetadata={siteMetadata} />
+            <Header siteMetadata={siteMetadata} isHome={isHome} />
             <main>
               {this.props.children()}
             </main>
