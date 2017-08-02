412f804264264eb9a5fb8981e3acc6ae4eebd13b
angeloocana <angeloocana@gmail.com>
2017-08-02 00:33:50 -0400

    Temp fix for en index

---
+++ src/pages/en.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import graphql from 'graphql';
+import PropTypes from 'proptypes';
+import Helmet from 'react-helmet';
+import PostList from '../components/PostList';
+import SocialLinks from '../components/SocialLinks';
+import Welcome from '../components/Welcome';
+
+class En extends React.Component {
+  static propTypes = {
+    data: PropTypes.object
+  }
+
+  render() {
+    const posts = this.props.data.allMarkdownRemark.edges;    
+    const { siteMetadata } = this.props.data.site;
+
+    return (
+      <div>
+        <Helmet title={siteMetadata.title} />
+        <SocialLinks />
+        <Welcome />
+        <PostList posts={posts} />
+      </div>
+    );
+  }
+}
+
+export default En;
+
+export const pageQuery = graphql`
+  query EnQuery {
+    site {
+      siteMetadata {
+        title
+        author {
+          homeCity,
+          name
+        }        
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      edges {
+        node{
+          frontmatter{
+            title,
+            tags,
+            date,
+            path
+          },
+          fields{
+            slug,
+            tagSlugs
+          },
+          excerpt 
+        }
+      }
+    }
+  }
+`;
