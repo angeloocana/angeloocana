c040c7c65ce6b999866cadaf3fd685ca204687e8
angeloocana <angeloocana@gmail.com>
2017-08-01 23:55:30 -0400

    Add pt index

--- src/pages/pt.js
+++ src/pages/pt.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import graphql from 'graphql';
+import Index from './index';
 
-const Pt = () => {
-  return (
-    <div>pt</div>
-  );
-};
+// const Pt = (props) => {
+//   console.log('props', props);
+//   return (
+//     <div>pt</div>
+//   );
+// };
 
-export default Pt;
+export default Index;
+
+export const pageQuery = graphql`
+  query PtIndexQuery {
+    site {
+      siteMetadata {
+        title
+        author {
+          homeCity,
+          name
+        }        
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      edges {
+        node{
+          frontmatter{
+            title,
+            tags,
+            date,
+            path
+          },
+          fields{
+            slug,
+            tagSlugs
+          },
+          excerpt 
+        }
+      }
+    }
+  }
+`;
