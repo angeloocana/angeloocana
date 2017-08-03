37e78fb6296aedf8c0350042023b9a4dc6ef170a
angeloocana <angeloocana@gmail.com>
2017-08-03 18:44:50 -0400

    Update french welcome

--- src/core/components/Welcome.jsx
+++ src/core/components/Welcome.jsx
@@ -18,7 +18,7 @@ const Wrapper = styled.section`
       line-height: ${props => props.theme.p.firstLetter.lineHeight};
       padding-top: ${props => props.theme.p.firstLetter.paddingTop};
   }
-`;
+`;diferent
 
 const getWelcomeMsg = () => {
   return {
@@ -27,7 +27,7 @@ const getWelcomeMsg = () => {
         Hi!!! Welcome to my site, where you can learn about Web, Apps and software development.
         I welcome you to accept my challenge: <mark>Study every day</mark>, at least 10 minutes. <br />
         Do NOT stop learning about tecnology, life, arts, languages, anything that you like.
-        Face your fears! Learning keep us young, it makes us see the universe in a diferent perspective and fall in love.
+        Face your fears! Learning keep us young, it makes us see the universe in a new perspective and fall in love.
         Are you ready to learn and change your life?
       </p>
     ),
@@ -36,16 +36,16 @@ const getWelcomeMsg = () => {
         Oi!!! Bem-vindo ao meu site, aqui você pode aprender sobre desenvolvimento de aplicações Web, Sites e Apps.
         Convido você a aceitar o meu desafio: <mark> Estude todos os dias </mark>, pelo menos 10 minutos. <br />
         NÃO pare de estudar sobre tecnologia, vida, artes, idiomas, qualquer coisa que você goste.
-        Enfrente seus medos! Aprender nos mantém jovens, faz-nos ver o universo em nova perspectiva diferente e se apaixonar.
+        Enfrente seus medos! Aprender nos mantém jovens, faz-nos ver o universo em nova perspectiva e se apaixonar.
         Você está pronto para aprender e mudar sua vida?
       </p>
     ),
     fr: (
       <p>
         Salut !!! Bienvenue sur mon site, ici vous pouvez en apprendre davantage sur le développement d'applications Web.
-        Je vous invite à accepter mon défi: <mark> Étude chaque jour </mark> au moins 10 minutes. <br />
-        PAS cesser d'étudier la technologie, la vie, les arts, les langues, tout ce que vous aimez.
-        Faites face à vos peurs! L'apprentissage nous garde jeunes, nous fait voir l'univers dans une nouvelle perspective différente et tombent amoureux.
+        Je vous invite à accepter mon défi: <mark> Étudie chaque jour </mark> au moins 10 minutes. <br />
+        Ne cessez pas d'étudier la technologie, la vie, les arts, les langues, tout ce que vous aimez.
+        Faites face à vos peurs! L'apprentissage nous garde jeune, nous fait voir l'univers d' une nouvelle perspective et nous fait tomber amoureux.
         Êtes-vous prêt à apprendre et à changer votre vie?
       </p>
     )
