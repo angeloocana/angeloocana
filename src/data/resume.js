8e6527b92bc373f3e3cd974a24414b2e17fddbbf
Angelo Ocana <angeloocana@gmail.com>
2017-08-31 21:10:19 -0400

    Add tech logos

--- src/data/resume.js
+++ src/data/resume.js
@@ -31,11 +31,11 @@ const technologies = {
     level: levels.expert,
     img: 'react.svg'
   },
-  'Angular': {
-    years: range(2014, 2016),
-    tags: [tags.frontEnd, tags.js],
-    level: levels.expert,
-    img: 'angular.svg'
+  'GraphQL': {
+    years: range(2016, thisYear),
+    tags: [tags.backEnd],
+    level: levels.proficient,
+    img: 'graphql.svg'
   },
   'NodeJs': {
     years: range(2015, thisYear),
@@ -49,6 +49,24 @@ const technologies = {
     level: levels.expert,
     img: 'javascript.png'
   },
+  'MongoDB': {
+    years: range(2015, thisYear),
+    tags: [tags.db, tags.noSql, tags.backEnd],
+    level: levels.proficient,
+    img: 'mongodb.svg'
+  },  
+  'Gatsby': {
+    years: range(2017, thisYear),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: 'gatsby.svg'
+  },
+  'Angular': {
+    years: range(2014, 2016),
+    tags: [tags.frontEnd, tags.js],
+    level: levels.expert,
+    img: 'angular.svg'
+  },
   'C#': {
     years: range(2008, 2016),
     tags: [tags.language, tags.net, tags.backEnd],
@@ -61,59 +79,47 @@ const technologies = {
     level: levels.expert,
     img: 'asp_net_mvc.svg'
   },
-  '.net WEP API': {
-    years: range(2008, 2016),
-    tags: [tags.net, tags.backEnd],
-    level: levels.expert,
-    img: ''
-  },
-  'Ajax': {
-    years: range(2011, thisYear),
+  'JQuery': {
+    years: range(2007, thisYear),
     tags: [tags.js, tags.frontEnd],
     level: levels.expert,
-    img: ''
-  },
-  'MongoDB': {
-    years: range(2015, thisYear),
-    tags: [tags.db, tags.noSql, tags.backEnd],
-    level: levels.proficient,
-    img: ''
-  },
+    img: 'jquery.gif'
+  },  
   'SQL Server': {
     years: range(2008, 2016),
     tags: [tags.db, tags.backEnd, tags.net],
     level: levels.expert,
-    img: ''
+    img: 'sql-server.svg'
   },
   'MySql': {
     years: range(2007, 2009),
     tags: [tags.db, tags.backEnd],
     level: levels.proficient,
-    img: ''
+    img: 'mysql.svg'
   },
   'PostgreSQL': {
     years: range(2014, 2015),
     tags: [tags.db, tags.backEnd],
     level: levels.proficient,
-    img: ''
-  },
-  'Access': {
-    years: range(2007, 2008),
-    tags: [tags.db, tags.backEnd, tags.net],
-    level: levels.novice,
-    img: ''
-  },
-  'VB.net': {
-    years: range(2007, 2008),
-    tags: [tags.language, tags.net, tags.backEnd],
-    level: levels.novice,
-    img: ''
+    img: 'postgresql.svg'
   },
   'PHP': {
     years: range(2008, 2010),
     tags: [tags.language, tags.backEnd],
     level: levels.proficient,
-    img: ''
+    img: 'php.png'
+  },
+  'Access': {
+    years: range(2007, 2008),
+    tags: [tags.db, tags.backEnd, tags.net],
+    level: levels.novice,
+    img: 'access.svg'
+  },
+  'VB.net': {
+    years: range(2007, 2008),
+    tags: [tags.language, tags.net, tags.backEnd],
+    level: levels.novice,
+    img: 'vb.svg'
   }
 };
 
