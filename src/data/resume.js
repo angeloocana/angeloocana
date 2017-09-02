fbd9bd0f5e194bf506ae17b7c34206257c7e0a44
Angelo Ocana <angeloocana@gmail.com>
2017-09-02 13:51:30 -0400

    Remove resume tags .net js nosql

--- src/data/resume.js
+++ src/data/resume.js
@@ -14,10 +14,6 @@ const levels = {
 
 const tags = {
   db: 'db',
-  noSql: 'NoSQL',
-  language: 'language',
-  net: '.net',
-  js: 'js',
   frontEnd: 'frontEnd',
   backEnd: 'backEnd'
 };
@@ -27,13 +23,13 @@ const thisYear = (new Date()).getFullYear();
 const technologies = {
   'Javascript': {
     years: range(2007, thisYear),
-    tags: [tags.language, tags.js, tags.frontEnd, tags.backEnd],
+    tags: [tags.frontEnd, tags.backEnd],
     level: levels.expert,
     img: 'javascript.png'
   },
   'React': {
     years: range(2016, thisYear),
-    tags: [tags.frontEnd, tags.js],
+    tags: [tags.frontEnd],
     level: levels.expert,
     img: 'react.svg'
   },
@@ -45,13 +41,13 @@ const technologies = {
   },
   'NodeJs': {
     years: range(2015, thisYear),
-    tags: [tags.backEnd, tags.js],
+    tags: [tags.backEnd],
     level: levels.expert,
     img: 'nodejs.svg'
   },
   'MongoDB': {
     years: range(2015, thisYear),
-    tags: [tags.db, tags.noSql, tags.backEnd],
+    tags: [tags.db, tags.backEnd],
     level: levels.proficient,
     img: 'mongodb.svg'
   },  
@@ -93,13 +89,13 @@ const technologies = {
   // },  
   'C#': {
     years: range(2008, 2016),
-    tags: [tags.language, tags.net, tags.backEnd],
+    tags: [tags.backEnd],
     level: levels.expert,
     img: 'csharp.svg'
   },
   '.net MVC': {
     years: range(2008, 2016),
-    tags: [tags.net, tags.backEnd],
+    tags: [tags.backEnd],
     level: levels.expert,
     img: 'asp_net_mvc.svg'
   },
@@ -111,13 +107,13 @@ const technologies = {
   // },
   'JQuery': {
     years: range(2007, thisYear),
-    tags: [tags.js, tags.frontEnd],
+    tags: [tags.frontEnd],
     level: levels.expert,
     img: 'jquery.gif'
   },  
   'SQL Server': {
     years: range(2008, 2016),
-    tags: [tags.db, tags.backEnd, tags.net],
+    tags: [tags.db, tags.backEnd],
     level: levels.expert,
     img: 'sql-server.svg'
   },
@@ -135,19 +131,19 @@ const technologies = {
   },
   'PHP': {
     years: range(2008, 2010),
-    tags: [tags.language, tags.backEnd],
+    tags: [tags.backEnd],
     level: levels.proficient,
     img: 'php.png'
   },
   'Access': {
     years: range(2007, 2008),
-    tags: [tags.db, tags.backEnd, tags.net],
+    tags: [tags.db, tags.backEnd],
     level: levels.novice,
     img: 'access.svg'
   },
   'VB.net': {
     years: range(2007, 2008),
-    tags: [tags.language, tags.net, tags.backEnd],
+    tags: [tags.backEnd],
     level: levels.novice,
     img: 'vb.svg'
   }
