aaa66ef4af2bac957178814ea77a5969bd84f02e
Angelo Ocana <angeloocana@gmail.com>
2017-09-11 19:35:58 -0400

    Resume: Add jobs and clients

--- src/data/resume/jobsAndClients.js
+++ src/data/resume/jobsAndClients.js
@@ -1,21 +1,642 @@
-// const {range, thisYear} = require('./time');
+const { technologies } = require('./technologies');
+
+const t = technologies;
 
 const jobsAndClients = {
-//   '': {
-//     years: range(),
-//     img: '',
-//     link: ''
-//   },
-//   '': {
-//     years: range(),
-//     img: '',
-//     link: ''
-//   },
-//   '': {
-//     years: range(),
-//     img: '',
-//     link: ''
-//   },
+  'Ângelo Ocanã Consultoria e Treinamentos': {
+    date: {
+      start: '2007-02-01',
+      end: null
+    },
+    img: '',
+    link: 'https://angeloocana.com',
+    description: {
+      'pt': 'Desenvolvi sistemas e sites para: Envio de SMS, Academias, E-commerce, Sistes Institucionais, entre outros.'
+    },    
+    projects: [
+      {
+        name: 'angeloocana.com',
+        description: '',
+        link: 'https://angeloocana.com',
+        technologies: [
+          t.gatsby,
+          t.graphql,
+          t.react,
+          t.styledComponents,
+          t.nodejs,
+          t.html,
+          t.css,
+          t.js,
+          t.vim,
+          t.vscode,
+          t.jest,
+          t.eslint,
+          t.babel,
+          t.surge
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Staffgeist',
+        description: '',
+        link: '',
+        technologies: [
+          t.angular,
+          t.ionic,
+          t.cordova,
+          t.cSharp,
+          t.typeScript,
+          t.babel,
+          t.gulp,
+          t.html,
+          t.css,
+          t.js,
+          t.vscode,
+          t.visualStudio,
+          t.jasmine,
+          t.tslint,
+          t.mongodb,
+          t.azure
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Hive',
+        description: '',
+        link: '',
+        technologies: [
+          t.angular,
+          t.ionic,
+          t.cordova,
+          t.cSharp,
+          t.typeScript,
+          t.babel,
+          t.gulp,
+          t.html,
+          t.css,
+          t.js,
+          t.vscode,
+          t.visualStudio,
+          t.jasmine,
+          t.tslint,
+          t.mongodb,
+          t.azure,
+          t.d3
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Jump Over Jump',
+        description: '',
+        link: '',
+        technologies: [
+          t.angular,
+          t.ionic,
+          t.cordova,
+          t.cSharp,
+          t.typeScript,
+          t.babel,
+          t.gulp,
+          t.html,
+          t.css,
+          t.js,
+          t.vscode,
+          t.visualStudio,
+          t.jasmine,
+          t.tslint,
+          t.mongodb,
+          t.azure
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'angeloocana.com.br',
+        description: '',
+        link: '',
+        technologies: [
+          t.angular,
+          t.cSharp,
+          t.typeScript,
+          t.babel,
+          t.gulp,
+          t.html,
+          t.css,
+          t.js,
+          t.vscode,
+          t.visualStudio,
+          t.jasmine,
+          t.tslint,
+          t.azure
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Emporio Viña Real',
+        description: '',
+        link: '',
+        technologies: [
+          t.html,
+          t.css,
+          t.js,
+          t.vscode
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'APM',
+        description: '',
+        link: '',
+        technologies: [
+          t.cSharp,
+          t.html,
+          t.css,
+          t.js,
+          t.jquery,
+          t.netMvc,
+          t.wcf,
+          t.sqlServer,
+          t.visualStudio,
+          t.entityFramework
+        ],
+        clients: [
+          {
+            name: 'APM',
+            link: ''
+          },
+          {
+            name: 'Sorocabacom',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'SMS BBM Brasil',
+        description: '',
+        link: '',
+        technologies: [
+          t.cSharp,
+          t.html,
+          t.css,
+          t.js,
+          t.jquery,
+          t.netMvc,
+          t.sqlServer,
+          t.visualStudio,
+          t.structureMap,
+          t.entityFramework
+        ],
+        clients: [
+          {
+            name: 'BBM Brasil',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'INNOVATIVE',
+        description: '',
+        link: '',
+        technologies: [
+          t.html,
+          t.css,
+          t.js,
+          t.vscode
+        ],
+        clients: [
+          {
+            name: 'INNOVATIVE',
+            link: ''
+          },
+          {
+            name: 'BBM Brasil',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Igreja Cristã de Alphaville',
+        description: '',
+        link: '',
+        technologies: [
+          t.html,
+          t.css,
+          t.js,
+          t.php,
+          t.mySql,
+          t.jquery,
+          t.dreamWeaver,
+          t.flash
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'Igreja Cristã de Maia',
+        description: '',
+        link: '',
+        technologies: [
+          t.adobeFlex
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      {
+        name: 'FRP',
+        description: '',
+        link: '',
+        technologies: [
+          t.html,
+          t.css,
+          t.js,
+          t.php,
+          t.mySql,
+          t.jquery,
+          t.dreamWeaver,
+          t.flash
+        ],
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      },
+      //   {
+      //     name: '',
+      //     description: '',
+      //     link: '',
+      //     clients: [
+      //       {
+      //         name: '',
+      //         link: ''
+      //       }
+      //     ]
+      //   },
+      {
+        name: 'Cadastro e Acesso de alunos',
+        description: '',
+        link: '',
+        technologies: [
+          t.php,
+          t.html,
+          t.css,
+          t.js,
+          t.jquery,
+          t.mySql,
+          t.dreamweaver
+        ],
+        clients: [
+          {
+            name: 'Academia Corpus Santana',
+            link: ''
+          }
+        ]
+      },      
+    ]
+  },
+  'Upgrade Consultoria': {
+    date: {
+      start: '2008-10-28',
+      end: '2010-06-11'
+    },
+    img: '',
+    link: '',
+    technologies: [
+      t.cSharp,
+      t.webForms,
+      t.windowsForms,
+      t.telerik,
+      t.flash,
+      t.html,
+      t.css,
+      t.js,
+      t.jquery,
+      t.sqlServer,
+      t.visualStudio
+    ],
+    description: {
+      'pt': `PriManager [Sistema de Gerenciamento de Obras da Sistema Pri]
+        - Sistema web para cadastramento das obras para os principais clientes: Eletropaulo, CDHU, entre outros.
+        
+        FDE [Fundação para o Desenvolvimento da Educação]
+        - Sistema para gerenciamento de infra-estrutura das escolas do Estado de São Paulo.
+        
+        ERP / CRM para controle de todas as operações da empresa: fluxo de caixa, cadastro e acompanhamento de relacionamento com clientes e fornecedores, entre outros módulos.
+        
+        Principais Tecnologias: .NET 2.0 / 3.5 utilizando linguagem C#, construção de aplicações web 2.0 com Telerik, Web Services, Windows Services, Flash / Action Script, Google Maps API, Banco de Dados: SQL Server 2005 / 2008`
+    },
+    projects: [
+      {
+        name: 'PriManager',
+        description: 'Sistema de Gerenciamento de Obras',
+        link: 'http://www.sistemapri.com.br/en/primanager.html',
+        clients: [
+          {
+            name: 'Sistema Pri',
+            link: 'http://www.sistemapri.com.br'
+          }
+        ]
+      },
+      {
+        name: 'FDE',
+        description: 'Sistema para gerenciamento de infra-estrutura das escolas do Estado de São Paulo',
+        link: 'http://www.fde.sp.gov.br',
+        clients: [
+          {
+            name: 'FDE',
+            link: 'http://www.fde.sp.gov.br'
+          }
+        ]
+      },
+      {
+        name: 'Mapa de Obras',
+        description: '',
+        link: 'https://www.aeseletropaulo.com.br',
+        clients: [
+          {
+            name: 'AES Eletropaulo',
+            link: 'https://www.aeseletropaulo.com.br'
+          }
+        ]
+      },
+      {
+        name: 'ERP / CRM',        
+        description: 'ERP / CRM para controle de todas as operações da empresa: fluxo de caixa, cadastro e acompanhamento de relacionamento com clientes e fornecedores, entre outros módulos.',
+        link: null,
+        clients: []
+      }
+    ]
+  },
+  'Media W': {
+    date: {
+      start: '2010-06-14',
+      end: '2010-07-22'
+    },
+    img: '',
+    link: 'http://www.mediaw.com.br/',
+    technologies: [
+      t.cSharp,
+      t.windowsForms,
+      t.mySql,
+      t.vs
+    ],
+    description: `Desenvolvimento de software para automação comercial PDV TED das lojas "Cadê o Nenê", impressora fiscal e máquina de cartão Bematech.
+    
+        Principais Tecnologias: .NET 3.5 utilizando linguagem C# para Windows Forms, Banco de Dados: MySQL`,
+    projects: [
+      {
+        name: 'PDV TED',
+        description: 'software para automação comercial PDV TED das lojas "Cadê o Nenê", impressora fiscal e máquina de cartão Bematech',
+        link: null,
+        clients: [
+          {
+            name: 'Cadê o Nenê',
+            link: ''
+          }
+        ]
+      }
+    ]
+  },
+  'Empresário Cobrança': {
+    date: {
+      start: '2010-07-23',
+      end: '2011-03-18'
+    },
+    img: 'empresario-cobranca.gif',
+    link: 'http://www.empresariocobranca.com.br/',
+    technologies: [
+      t.cSharp,
+      t.webForms,
+      t.wpf,
+      t.ext,
+      t.html,
+      t.css,
+      t.js,
+      t.jquery,
+      t.sqlServer,
+      t.visualStudio
+    ],
+    description: `Sistema para negociação de dívidas de pessoa física e jurídica de todo o Brasil, utilizando complexos cálculos financeiros, gerador do algoritmo do código de barras, business inteligence para importação e tratamento de milhões de registros em diversas extensões e formatação de arquivo.
+    Sistema para controle de acesso com captura de foto utilizando WPF.
+    Sistema de Recursos Humanos interno para os colaboradores.
+    Sistema para controle financeiro e CRM
+    
+    Principais Tecnologias: WCF, WPF, NET 3.5 utilizando linguagem C#, WebForms, Windows Forms, Windows Services,  EXT.Net
+    Banco de Dados: SQL Server (Criação de Querys, Stored Procedures, Functions, Views e Jobs com alta performance para suportar a grande massa de dados )`,
+    projects: [
+      {
+        name: '',
+        description: '',
+        link: null,
+        clients: [
+          {
+            name: '',
+            link: ''
+          }
+        ]
+      }
+    ]
+  },
+  'Total On Demand': {
+    date: {
+      start: '2011-04-18',
+      end: '2015-03-17'
+    },
+    img: 'tod.png',
+    link: 'http://www.totalondemand.com.br/',
+    technologies: [
+      t.cSharp,
+      t.html,
+      t.css,
+      t.js,
+      t.jquery,
+      t.netMvc,
+      t.webForms,
+      t.wcf,
+      t.sqlServer,
+      t.visualStudio,
+      t.structureMap,
+      t.entityFramework
+    ],
+    description: `Desenvolvimento de sites para campanhas de incentivo, endomarketing e premiação online para os clientes Vivo|Telefonica, BASF, Net, Volkswagen, FMC, FTD, Sky, OI, BRF, Loreal, Johnson, Sony, Syngenta, Cury e Tenda. As campanhas possuíam uma base de participantes e os mesmos poderiam interagir entre si através de uma rede social, jogar, visualizar suas metas e resultados, feed de notícias, quiz, galeria de fotos, entre outras.`,
+    projects: [
+      {
+        name: '',
+        description: 'Desenvolvimento de sites para campanhas de incentivo, endomarketing e premiação online para os clientes Vivo|Telefonica, BASF, Net, Volkswagen, FMC, FTD, Sky, OI, BRF, Loreal, Johnson, Sony, Syngenta, Cury e Tenda. As campanhas possuíam uma base de participantes e os mesmos poderiam interagir entre si através de uma rede social, jogar, visualizar suas metas e resultados, feed de notícias, quiz, galeria de fotos, entre outras.',
+        link: null,
+        clients: [
+          {
+            name: 'Telefonica',
+            link: ''
+          },
+          {
+            name: 'Vivo',
+            link: ''
+          },
+          {
+            name: 'BASF',
+            link: ''
+          },
+          {
+            name: 'Net',
+            link: ''
+          },
+          {
+            name: 'Volkswagen',
+            link: ''
+          },
+          {
+            name: 'FMC',
+            link: ''
+          },
+          {
+            name: 'Tenda',
+            link: ''
+          },
+          {
+            name: 'FTD',
+            link: ''
+          },
+          {
+            name: 'Sky',
+            link: ''
+          },
+          {
+            name: 'OI',
+            link: ''
+          },
+          {
+            name: 'BRF',
+            link: ''
+          },
+          {
+            name: 'Loreal',
+            link: ''
+          },
+          {
+            name: 'Johnson',
+            link: ''
+          },
+          {
+            name: 'Sony',
+            link: ''
+          },
+          {
+            name: 'Syngenta',
+            link: ''
+          },
+          {
+            name: 'Cury',
+            link: ''
+          }
+        ]
+      }
+    ]
+  },
+  'POL': {
+    date: {
+      start: '2011-04-18',
+      end: '2015-03-17'
+    },
+    img: 'pol.png',
+    link: 'http://pol.com.br',
+    needWhiteBg: true,
+    technologies: [
+      t.cSharp,
+      t.html,
+      t.css,
+      t.js,
+      t.jquery,
+      t.netMvc,
+      t.postgres,
+      t.webForms,
+      t.wcf,
+      t.sqlServer,
+      t.visualStudio,
+      t.lucene,
+      t.rabbitmq,
+      t.structureMap,
+      t.entityFramework
+    ],
+    description: `POL (Prêmios Online), plataforma de e-commerce para resgate de produtos / serviços através da pontuação adquirida na apuração das metas e resultados. Integração com parceiros B2W, Magazine Luiza, Walmart, Casas Bahia, FastShop, entre outros.
+        
+        Criação de Engine de busca utilizando Lucene.
+        
+        Principais Tecnologias: MVC, API, Jquery, Ajax, .NET 4.0 / 4.5 utilizando linguagem C#, Entity Framework, Migrations, UnitTest, Lucene, StructureMap, WebForms
+        Principais Metodologias: Domain Driven Design, Test Driven Development, Scrum
+        Banco de Dados: SQL Server (Gerenciamento, Criação de Querys, Stored Procedures, Functions, Views e Jobs)`,
+    projects: [
+      {
+        name: '',
+        description: '',
+        link: null,
+        clients: [
+          {
+            name: 'B2W',
+            link: ''
+          },
+          {
+            name: 'Magazine Luiza',
+            link: ''
+          },
+          {
+            name: 'Walmart',
+            link: ''
+          },
+          {
+            name: 'Avon',
+            link: ''
+          },
+          {
+            name: 'Casas Bahia',
+            link: ''
+          },
+          {
+            name: 'FastShop',
+            link: ''
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = {
--- src/data/resume/technologies.js
+++ src/data/resume/technologies.js
@@ -1,4 +1,4 @@
-const {range, thisYear} = require('./time');
+const { range, thisYear } = require('./time');
 
 const levels = {
   novice: 'novice',
@@ -15,49 +15,56 @@ const tags = {
 };
 
 const technologies = {
-  'Javascript': {
+  js: {
+    name: 'Javascript',
     years: range(2007, thisYear),
     tags: [tags.frontEnd, tags.backEnd],
     level: levels.expert,
     img: 'javascript.png',
     link: 'https://en.wikipedia.org/wiki/JavaScript'
   },
-  'React': {
+  react: {
+    name: 'React',
     years: range(2016, thisYear),
     tags: [tags.frontEnd],
     level: levels.expert,
     img: 'react.svg',
     link: 'https://facebook.github.io/react/'
   },
-  'GraphQL': {
+  'graphql': {
+    name: 'GraphQL',
     years: range(2016, thisYear),
     tags: [tags.backEnd],
     level: levels.proficient,
     img: 'graphql.svg',
     link: 'http://graphql.org/'
   },
-  'NodeJs': {
+  nodejs: {
+    name: 'NodeJs',
     years: range(2015, thisYear),
     tags: [tags.backEnd],
     level: levels.expert,
     img: 'nodejs.svg',
     link: 'https://nodejs.org'
   },
-  'MongoDB': {
+  mongodb: {
+    name: 'MongoDB',
     years: range(2015, thisYear),
     tags: [tags.db, tags.backEnd],
     level: levels.proficient,
     img: 'mongodb.svg',
     link: 'https://www.mongodb.com/'
   },
-  'Gatsby': {
+  gatsby: {
+    name: 'Gatsby',
     years: range(2017, thisYear),
     tags: [tags.frontEnd],
     level: levels.proficient,
     img: 'gatsby.svg',
     link: 'https://www.gatsbyjs.org/'
   },
-  'Ramda': {
+  ramda: {
+    name: 'Ramda',
     years: range(2017, thisYear),
     tags: [tags.frontEnd, tags.backEnd],
     level: levels.proficient,
@@ -65,7 +72,8 @@ const technologies = {
     needWhiteBg: true,
     link: 'http://ramdajs.com/'
   },
-  'Docker': {
+  docker: {
+    name: 'Docker',
     years: range(2017, thisYear),
     tags: [tags.backEnd],
     level: levels.proficient,
@@ -73,91 +81,296 @@ const technologies = {
     needWhiteBg: true,
     link: 'https://www.docker.com/'
   },
-  'Arch Linux': {
+  typeScript: {
+    name: 'TypeScript',
+    years: range(2015, thisYear),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  tslint: {
+    name: 'TSLint',
+    years: range(2015, thisYear),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  gulp: {
+    name: 'Gulp',
+    years: range(2015, 2016),
+    tags: [tags.frontEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  jasmine: {
+    name: 'Jasmine',
+    years: range(2015, 2016),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  arch: {
+    name: 'Arch Linux',
     years: range(2015, thisYear),
     tags: [tags.os],
     level: levels.proficient,
     img: 'archlinux.svg',
     link: 'https://www.archlinux.org/'
   },
-  'Debian': {
+  debian: {
+    name: 'Debian',
     years: range(2014, 2016),
     tags: [tags.os],
     level: levels.proficient,
     img: 'debian.png',
     link: 'https://www.debian.org/'
   },
-  'Vim': {
+  vim: {
+    name: 'Vim',
     years: range(2015, thisYear),
     tags: [tags.textEditor],
     level: levels.proficient,
     img: 'vim.svg',
     link: 'https://vim.sourceforge.io'
   },
-  'vs code': {
+  vscode: {
+    name: 'vs code',
     years: range(2015, thisYear),
     tags: [tags.textEditor],
     level: levels.expert,
     img: 'vscode.png',
     link: 'https://code.visualstudio.com/'
   },
-  'Angular': {
+  angular: {
+    name: 'Angular',
     years: range(2014, 2016),
     tags: [tags.frontEnd],
     level: levels.expert,
     img: 'angular.svg',
     link: 'https://angularjs.org/'
   },
-  'C#': {
+  ionic: {
+    name: 'Ionic',
+    years: range(2015, 2016),
+    tags: [tags.frontEnd, tags.mobile],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  cordova: {
+    name: 'Cordova',
+    years: range(2015, thisYear),
+    tags: [tags.frontEnd, tags.mobile],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  azure: {
+    name: 'Azure',
+    years: range(2015, thisYear),
+    tags: [tags.cloud],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  amazon: {
+    name: 'Amazon',
+    years: range(2014, thisYear),
+    tags: [tags.cloud],
+    level: levels.novice,
+    img: '',
+    link: ''
+  },
+  eroku: {
+    name: 'Eroku',
+    years: range(2014, 2015),
+    tags: [tags.cloud],
+    level: levels.novice,
+    img: '',
+    link: ''
+  },
+  surge: {
+    name: 'Surge',
+    years: range(2017, thisYear),
+    tags: [tags.cloud],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  cSharp: {
+    name: 'C#',
     years: range(2008, 2016),
     tags: [tags.backEnd],
     level: levels.expert,
     img: 'csharp.svg',
     link: 'https://en.wikipedia.org/wiki/C_Sharp_(programming_language)'
   },
-  '.net MVC': {
+  netMvc: {
+    name: '.net MVC',
     years: range(2010, 2016),
     tags: [tags.frontEnd, tags.backEnd],
     level: levels.expert,
     img: 'asp_net_mvc.svg',
     link: 'https://www.asp.net/mvc'
   },
-  'web forms': {
+  webForms: {
+    name: 'web forms',
     years: range(2008, 2012),
     tags: [tags.frontEnd, tags.backEnd],
     level: levels.expert,
     img: 'webforms.png',
     link: 'https://www.asp.net/web-forms'
   },
-  'visual studio': {
+  windowsForms: {
+    name: 'windows forms',
+    years: range(2008, 2012),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  wpf: {
+    name: 'WPF',
+    years: range(2010, 2011),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  lucene: {
+    name: 'Lucene',
+    years: range(2014, 2015),
+    tags: [tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  rabbitmq: {
+    name: 'RabbitMQ',
+    years: range(2014, 2015),
+    tags: [tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  structureMap: {
+    name: 'StructureMap',
+    years: range(2014, 2016),
+    tags: [tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  entityFramework: {
+    name: 'Entity Framework',
+    years: range(2012, 2016),
+    tags: [tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  wcf: {
+    name: 'WCF',
+    years: range(2011, 2015),
+    tags: [tags.backEnd],
+    level: levels.expert,
+    img: '',
+    link: ''
+  },
+  ext: {
+    name: 'EXT .net',
+    years: range(2010, 2011),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  d3: {
+    name: 'D3',
+    years: range(2016, thisYear),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  styledComponents: {
+    name: 'Styled Components',
+    years: range(2017, thisYear),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  jest: {
+    name: 'Jest',
+    years: range(2017, thisYear),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  eslint: {
+    name: 'ESlint',
+    years: range(2016, thisYear),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  babel: {
+    name: 'babel',
+    years: range(2016, thisYear),
+    tags: [tags.frontEnd, tags.backEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  visualStudio: {
+    name: 'visual studio',
     years: range(2007, 2016),
     tags: [tags.textEditor],
     level: levels.expert,
     img: 'vs.svg',
     link: 'https://www.visualstudio.com/'
   },
-  'JQuery': {
+  dreamWeaver: {
+    name: 'Dreamweaver',
+    years: range(2007, 2016),
+    tags: [tags.textEditor],
+    level: levels.expert,
+    img: 'vs.svg',
+    link: 'https://www.visualstudio.com/'
+  },  
+  jquery: {
+    name: 'JQuery',
     years: range(2008, thisYear),
     tags: [tags.frontEnd],
     level: levels.expert,
     img: 'jquery.svg',
     link: 'https://jquery.com/'
   },
-  'HTML': {
+  html: {
+    name: 'HTML',
     years: range(2007, thisYear),
     tags: [tags.frontEnd],
     level: levels.expert,
     img: 'html.png',
     link: 'https://www.w3schools.com/html/'
   },
-  'CSS': {
+  css: {
+    name: 'CSS',
     years: range(2007, thisYear),
     tags: [tags.frontEnd],
     level: levels.expert,
     img: 'css.svg',
     link: 'https://www.w3schools.com/css/'
   },
-  'SQL Server': {
+  sqlServer: {
+    name: 'SQL Server',
     years: range(2008, 2016),
     tags: [tags.db, tags.backEnd],
     level: levels.expert,
@@ -165,41 +378,70 @@ const technologies = {
     needWhiteBg: true,
     link: 'https://en.wikipedia.org/wiki/Microsoft_SQL_Server'
   },
-  'MySql': {
+  mySql: {
+    name: 'MySql',
     years: range(2007, 2009),
     tags: [tags.db, tags.backEnd],
     level: levels.proficient,
     img: 'mysql.png',
     link: 'https://www.mysql.com/'
   },
-  'PostgreSQL': {
+  postgres: {
+    name: 'PostgreSQL',
     years: range(2014, 2015),
     tags: [tags.db, tags.backEnd],
     level: levels.proficient,
     img: 'postgresql.svg',
     link: 'https://www.postgresql.org/'
   },
-  'PHP': {
+  php: {
+    name: 'PHP',
     years: range(2008, 2010),
     tags: [tags.backEnd],
     level: levels.proficient,
     img: 'php.png',
     link: 'http://php.net/'
   },
-  'Access': {
+  access: {
+    name: 'Access',
     years: range(2007, 2008),
     tags: [tags.db, tags.backEnd],
     level: levels.novice,
     img: 'access.svg',
     link: 'https://en.wikipedia.org/wiki/Microsoft_Access'
   },
-  'VB.net': {
+  vb: {
+    name: 'VB .net',
     years: range(2007, 2008),
     tags: [tags.backEnd],
     level: levels.novice,
     img: 'vb.svg',
     link: 'https://en.wikipedia.org/wiki/Visual_Basic_.NET'
   },
+  telerik: {
+    name: 'Telerik',
+    years: range(2008, 2010),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  flash: {
+    name: 'Flash / Action Script',
+    years: range(2007, 2010),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  },
+  adobeFlex: {
+    name: 'Adobe Flex Builder',
+    years: range(2008, 2009),
+    tags: [tags.frontEnd],
+    level: levels.proficient,
+    img: '',
+    link: ''
+  }  
   // 'Gimp': {
   //   years: range(2007, 2008),
   //   tags: [tags.backEnd],
