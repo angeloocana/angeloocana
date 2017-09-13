817a6e5768eef1e1294329e1edc3ff7d9508d441
Angelo Ocana <angeloocana@gmail.com>
2017-09-12 23:04:23 -0400

    Add intl

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,6 +11,11 @@ import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
 import pt from 'react-intl/locale-data/pt';
 
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/fr';
+import 'intl/locale-data/jsonp/pt';
+
 import {
   getLangs,
   getUrlForLang,
