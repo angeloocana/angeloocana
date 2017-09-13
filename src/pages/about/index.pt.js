d2065fc429bdca0bc76902fa0875c1b5e1bc7f2d
Angelo Ocana <angeloocana@gmail.com>
2017-09-13 19:23:47 -0400

    Update about me pt

--- src/pages/about/index.pt.js
+++ src/pages/about/index.pt.js
@@ -5,16 +5,15 @@ const i18n = {
   title: 'Sobre mim',
   description: (
     <p>
-      OI! Eu sou o Ângelo! Sou Desenvolvedor Full-Stack desde 2007. <br />
-      Amo Linux, technologias Open Source, minha filha e minha mulher. <br />
-      Hoje uso React, Nodejs e Arch linux para desenvolvimento de Apps Web e Mobile.
+      Oi! Eu sou o Ângelo! Desenvolvedor Full-Stack desde 2007. <br />
+      Amo Linux e tecnologias Open Source tanto quanto amo minha filha e esposa. <br />
+      Atualmente uso React, Nodejs e Arch linux para desenvolvimento de Apps Web e Mobile.
     </p>
   ),
   descriptionForGoogle: `
-    Oi! Eu sou o Ângelo! Desenvolvedor Full-Stack desde 2007. 
-Amo Linux e tecnologias Open Source tanto quanto amo minha filha e esposa. 
-Atualmente uso React, Nodejs e Arch linux para desenvolvimento de Apps Web e Mobile.
-
+    Oi! Eu sou o Ângelo! Desenvolvedor Full-Stack desde 2007.
+    Amo Linux e tecnologias Open Source tanto quanto amo minha filha e esposa.
+    Atualmente uso React, Nodejs e Arch linux para desenvolvimento de Apps Web e Mobile.
   `
 };
 
