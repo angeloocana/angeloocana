3b29ed57cc937216f9d99730b69e1d5f0a5b3f8c
Angelo Ocana <angeloocana@gmail.com>
2017-09-20 11:10:42 -0400

    Update about me

--- src/pages/about/index.en.js
+++ src/pages/about/index.en.js
@@ -4,15 +4,21 @@ import AboutMe from './_index';
 const i18n = {
   description: (
     <p>
-      Hi! I am Ângelo, a Freelance Full-Stack Developer with a Global mindset since 2007. <br />
-      I love Linux and Open Source technologies as much as my wife and daughter. <br />
-      Today I'm using React, Nodejs and Arch linux for development of Web and Mobile Apps.
+      Hi! I am Ângelo, a Freelance Full-Stack Developer since 2007.
+      My professional journey started when I was 14 years-old. 
+      Since then, I have been passionate about programming.  <br /><br />
+      
+      Most recently, I have my mindset on TDD, Functional Programming, Offline First, Mobile First
+      and Frog Cloud. <br /><br />
+
+      I’m an Arch Linux user and Nodejs Developer.
     </p>
   ),
   descriptionForGoogle: `
-    Hi! I am Ângelo, a Freelance Full-Stack Developer with a Global mindset since 2007.
-    I love Linux and Open Source technologies as much as my wife and daughter.
-    Today I'm using React, Nodejs and Arch linux for development of Web and Mobile Apps.
+    My professional journey started when I was 14 years-old. Since then, I have been passionate about programming. Most recently, I have my mindset on TDD, Functional Programming, Offline First, Mobile First 
+    and Frog Cloud.
+    
+    I’m an Arch Linux user.
   `
 };
 
