5580e7393ec496ea49707325ac4d4721bffb0387
Angelo Ocana <angeloocana@gmail.com>
2017-10-12 16:35:52 -0400

    Redirect user to languague home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Index from '../components/IndexPage';
 import graphql from 'graphql';
+import { redirectToHome } from 'ptz-i18n';
 
-export default (props) => <Index {...props} />;
+class RedirectIndex extends React.PureComponent {
+  constructor(args) {
+    super(args);
+    const { langs, defaultLangKey } = args.data.site.siteMetadata.languages;
+    redirectToHome(langs, defaultLangKey);
+  }
+
+  render() {
+    return (<Index {...this.props} />);
+  }
+}
+
+RedirectIndex.propTypes = {
+
+};
+
+export default RedirectIndex;
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -30,6 +47,10 @@ export const pageQuery = graphql`
     }
     site{
       siteMetadata{
+        languages {
+          defaultLangKey
+          langs
+        }
         resume{
           technologies{
             name,
