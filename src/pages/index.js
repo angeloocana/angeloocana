ac7006752a20258ad84ef87ca3db80e966369b92
Angelo Ocana <angeloocana@gmail.com>
2017-10-13 14:52:30 -0400

    Remove redirecting msg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Index from '../components/IndexPage';
 import graphql from 'graphql';
 import { redirectToHome } from 'ptz-i18n';
 
@@ -11,50 +10,19 @@ class RedirectIndex extends React.PureComponent {
   }
 
   render() {
-    return (<Index {...this.props} />);
+    return (<div />);
   }
 }
 
 export default RedirectIndex;
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 3
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: {
-        frontmatter: { draft: { ne: true } },
-        fields: { langKey: { regex: "/(en|any)/" } }
-      },
-    ) {
-      edges {
-        node{
-          frontmatter{
-            title,
-            date
-          },
-          fields{
-            slug,
-            langKey
-          },
-          excerpt
-        }
-      }
-    }
+  query IndexQuery {    
     site{
       siteMetadata{
         languages {
           defaultLangKey
           langs
-        }
-        resume{
-          technologies{
-            name,
-            tags,
-            level,
-            years,
-            img
-          }
         }
       }
     }
